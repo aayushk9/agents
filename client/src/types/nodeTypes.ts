f2728482d9e5fbdaf8a2e1c25d6bc78544a49e65
aayushk9 <ayushkokate1@gmail.com>
2025-05-03 02:24:03 +0530

    feat(node): add node type for agents and tools

---
+++ client/src/types/nodeTypes.ts
@@ -0,0 +1,7 @@
+import { AgentNode } from "../components/nodes/AgentNode";
+import { ToolNode } from "../components/nodes/ToolNode";
+
+export const nodeTypes = {
+    agentNode: AgentNode,
+    toolNode: ToolNode
+}
