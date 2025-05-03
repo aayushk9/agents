7ed48cd7ecfbf2d2f7711807bf8ea90ac6c2a7ba
aayushk9 <ayushkokate1@gmail.com>
2025-05-03 19:24:30 +0530

    feat(pages): include home page

--- client/src/components/pages/homePage/index.tsx
+++ client/src/components/pages/homePage/index.tsx
@@ -1,10 +1,16 @@
 import { Navbar } from "../../ui/navbar"
+import { AgentNode } from "../../nodes/AgentNode"
+import { ToolNode } from "../../nodes/ToolNode"
 
 export function HomePage() {
     return (
         <>
           <div>
             <Navbar/>
+          </div>
+          <div>
+           <AgentNode data={{label: "NFT Agent"}}/>
+           <ToolNode data={{label: "API Fetchet"}}/>
           </div>
         </>
     )
